18d8ca4498e4a63c73e30a0691c33162322eafeb
karla <karla.cahaya@gmail.com>
2025-04-20 14:52:39 +0700

    chakra ui config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "./components/ui/provider";
+import { TypeAnimation } from "react-type-animation";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>
+);
